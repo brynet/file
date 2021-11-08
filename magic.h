9bf008a740c08292d515c74fcb36971336afdea1
Bryan Steele <brynet@gmail.com>
2021-11-08 16:11:10 -0500

    portable; workaround missing <endian.h> functions on macOS

--- magic.h
+++ magic.h
@@ -36,6 +36,26 @@
 #endif
 #define DEF_WEAK(_x)
 
+#ifdef __APPLE__
+#include <libkern/OSByteOrder.h>
+
+#define htobe16(x) OSSwapHostToBigInt16(x)
+#define htole16(x) OSSwapHostToLittleInt16(x)
+#define be16toh(x) OSSwapBigToHostInt16(x)
+#define le16toh(x) OSSwapLittleToHostInt16(x)
+
+#define htobe32(x) OSSwapHostToBigInt32(x)
+#define htole32(x) OSSwapHostToLittleInt32(x)
+#define be32toh(x) OSSwapBigToHostInt32(x)
+#define le32toh(x) OSSwapLittleToHostInt32(x)
+
+#define htobe64(x) OSSwapHostToBigInt64(x)
+#define htole64(x) OSSwapHostToLittleInt64(x)
+#define be64toh(x) OSSwapBigToHostInt64(x)
+#define le64toh(x) OSSwapLittleToHostInt64(x)
+
+#endif /* ! __APPLE__ */
+
 #ifndef HAVE_EXPLICIT_BZERO
 void explicit_bzero(void *_buf, size_t _len);
 #endif
